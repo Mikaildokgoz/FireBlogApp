402a750b5a2511aacc9aa3b1ae4f5f8d0a624ec2
ersen <70ozturkersen@gmail.com>
2022-04-22 16:51:06 +0300

    developing process ongoing

--- fire-blog-app/src/components/Navbar.js
+++ fire-blog-app/src/components/Navbar.js
@@ -1,37 +1,14 @@
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
 
-
-
-
-
-// const StyledInputBase = styled(InputBase)(({ theme }) => ({
-//   color: 'inherit',
-//   '& .MuiInputBase-input': {
-//     padding: theme.spacing(1, 1, 1, 0),
-//     // vertical padding + font size from searchIcon
-//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-//     transition: theme.transitions.create('width'),
-//     width: '100%',
-//     [theme.breakpoints.up('md')]: {
-//       width: '20ch',
-//     },
-//   },
-// }));
 
 export default function PrimarySearchAppBar() {
 
@@ -75,9 +52,11 @@ export default function PrimarySearchAppBar() {
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      <MenuItem onClick={handleMenuClose}>Login</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Register</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Login?</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Register?</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
       <MenuItem onClick={handleMenuClose}>New</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
     </Menu>
   );
 
@@ -102,7 +81,19 @@ export default function PrimarySearchAppBar() {
       <IconButton >
           <AccountCircle />
         </IconButton>
-        <p>Login</p>
+        <p>Login?</p>
+      </MenuItem>
+      <MenuItem>
+      <IconButton >
+          <AccountCircle />
+        </IconButton>
+        <p>Register?</p>
+      </MenuItem>
+      <MenuItem>
+      <IconButton >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
       </MenuItem>
 
       <MenuItem>
@@ -116,7 +107,7 @@ export default function PrimarySearchAppBar() {
         <IconButton >
           <AccountCircle />
         </IconButton>
-        <p>Profile</p>
+        <p>Logout</p>
       </MenuItem>
     </Menu>
   );
