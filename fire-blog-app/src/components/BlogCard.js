c33866c0f6127d6192cf9b44f6261e9b5fc6e871
mikaildokgoz <mikaildokgoz@gmail.com>
2022-04-23 16:22:24 +0300

    blog added and dashboard created

--- fire-blog-app/src/components/BlogCard.js
+++ fire-blog-app/src/components/BlogCard.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import { BlogContext } from "../contexts/BlogContext";
+// import { AuthContext } from "../contexts/AuthContext";
 
-const BlogCard = () => {
+export default function BlogCard({item}) {
+  // const { useFetch, item } = React.useContext(itemContext);
+  // const { currentUser } = React.useContext(AuthContext);
+  // useFetch();
+  console.log(item);
   return (
-    <div>BlogCard</div>
-  )
-}
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image={item?.url}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item?.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item?.addDate}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item?.content}
+        </Typography>
 
-export default BlogCard
+        <Typography variant="body2" color="text.secondary">
+          {item?.user}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
--- fire-blog-app/src/pages/Dashboard.js
+++ fire-blog-app/src/pages/Dashboard.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Box, Container, Typography } from "@mui/material";
+import React, { useContext } from "react";
+import BlogCard from "../components/BlogCard";
+import { BlogContext } from "../contexts/BlogContext";
 
 const Dashboard = () => {
+  const { useFetch, blog } = useContext(BlogContext);
+  useFetch();
+  console.log(blog);
   return (
-    <div>Dashboard</div>
-  )
-}
-
-export default Dashboard
+    <Container>
+      <Typography
+        gutterBottom
+        variant="h3"
+        component="div"
+        sx={{ fontFamily: "Girassol", textAlign: "center", color: "#046582" }}
+      >
+        ──── Dashboard ────
+      </Typography>
+      <Box
+        xs={{ d: "flex" }}
+        display="flex"
+        alignItems="center"
+        justifyContent="space-evenly"
+        flexWrap="wrap"
+      >
+        {blog?.map((item, index) => return ({
+          <BlogCard item={item} key={index} />;
+        }))}
+      </Box>
+    </Container>
+  );
+};
+export default Dashboard;
