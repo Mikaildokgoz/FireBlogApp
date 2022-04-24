5dcc59af0c15f3906a78fb955c5011227cc5d8b0
ersen <70ozturkersen@gmail.com>
2022-04-24 13:12:32 +0300

    update page created

--- fire-blog-app/src/pages/Details.js
+++ fire-blog-app/src/pages/Details.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { BlogContext } from "../contexts/BlogContext";
-// import * as React from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -18,24 +17,25 @@ import { AuthContext } from "../contexts/AuthContext";
 const Details = () => {
   const { EditBlog, DeleteBlog } = useContext(BlogContext);
   const { currentUser } = useContext(AuthContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const location = useLocation();
-  console.log(location);
-  console.log(location.state);
-  console.log(location.state.item);
-  console.log(location.state.item.id);
+ /*  console.log('location->>',location);
+  console.log('location.state->>',location.state);
+  console.log('location.state.item->>',location.state.item);
+  console.log('location.state.item.id->>',location.state.item.id);
+  console.log('currentUser.email->>',currentUser.email); */
 
   const item = location.state.item;
-  console.log(item);
 
-  const handleEdit = (id) => {
-    EditBlog(id);
+  const handleNavigateUpdate = (id) => {
+    // navigate('/updateblog')
+    navigate("/updateblog", { state: { item } })
   };
 
   const handleDelete = (id) => {
     DeleteBlog(id);
-    navigate('/')
+    navigate("/");
   };
 
   return (
@@ -79,7 +79,7 @@ const Details = () => {
         <Button onClick={() => handleDelete(item.id)}>Delete</Button>
       ) : null}
       {currentUser?.email === item?.user ? (
-        <Button onClick={handleEdit}>Edit</Button>
+        <Button onClick={() => handleNavigateUpdate(item.id)}>Edit</Button>
       ) : null}
     </Container>
   );
--- fire-blog-app/src/pages/UpdateBlog.js
+++ fire-blog-app/src/pages/UpdateBlog.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import Box from "@mui/material/Box";  
+import TextField from "@mui/material/TextField";
+import { useContext, useState } from "react";
+import blok from "../assets/blok.png";
+import { AuthContext } from "../contexts/AuthContext";
+import { BlogContext } from "../contexts/BlogContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const UpdateBlog = () => {
+export default function NewBlog() {
+  const { EditBlog } = useContext(BlogContext);
+  const {currentUser} = useContext(AuthContext)
+  const navigate = useNavigate();
+
+
+  
+  const location = useLocation();
+  const item = location.state.item
+
+  const [title, setTitle] = useState();
+  const [url, setUrl] = useState();
+  const [content, setContent] = useState();
+  const [posts, setPosts] = useState({
+    id: item.id,
+    title: item.title,
+    url: item.url,
+    content: item.content,
+    user: currentUser.email,
+    addDate: new Date(),
+    likeCount: 0,
+    commnetCount: 0
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPosts({ ...posts, [name]: value });
+  };
+
+  const handleSubmitUpdate = (e) => {
+    e.preventDefault(e);
+    setPosts({user: currentUser.email})
+    EditBlog(posts);
+    navigate("/");
+  };
+
   return (
-    <div>UpdateBlog</div>
-  )
+    <form action="" style={{ margin: "auto" }}>
+      <div>
+        <img src={blok} alt="" />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <TextField
+          onChange={handleChange}
+          name="title"
+          value={posts.title}
+          id="outlined-basic"
+          label="Title"
+          variant="outlined"
+        />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <TextField
+          onChange={handleChange}
+          name="url"
+          value={posts.url}
+          id="outlined-basic"
+          label="İmgUrl"
+          variant="outlined"
+        />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <TextField
+          onChange={handleChange}
+          name="content"
+          value={posts.content}
+          id="outlined-basic"
+          label="Context"
+          variant="outlined"
+        />
+      </div>
+      <div style={{ margin: "10px" }}>
+        <button onClick={handleSubmitUpdate} >
+          Update BLOG
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default UpdateBlog
+
