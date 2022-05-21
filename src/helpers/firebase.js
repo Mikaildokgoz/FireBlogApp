c6992ee06d54810f9cbd93cb0f828009cb7f9719
mikaildokgoz <mikaildokgoz@gmail.com>
2022-05-22 01:35:51 +0300

    firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -3,6 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDZbpl6I3u1oUyslT2ys9Yuz_v1MnYtlmA",
+//   authDomain: "fir-blogapp-fa64e.firebaseapp.com",
+//   projectId: "fir-blogapp-fa64e",
+//   storageBucket: "fir-blogapp-fa64e.appspot.com",
+//   messagingSenderId: "67991411370",
+//   appId: "1:67991411370:web:3ec2147122033ba56e8ed3"
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyCWuUCHCTgpQeDk17NpPSdjAmEUWePoUG8",
   authDomain: "aaaa-d3864.firebaseapp.com",
